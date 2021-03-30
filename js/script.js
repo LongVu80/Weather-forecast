0e1ac89ef0f739421fda1851b5e1cba82a73fcfa
LongVu80 <thienlong14th@gmail.com>
2021-03-30 18:42:32 -0500

    vietnamese version

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 $(document).ready(function(){
+    
     var apiKey = '6b71e45e81339efb68c7b8d995519887'
-
-
     $('form').submit(function () {
         var city = $('#city').val();
         var cityString = `${city}`
 
-        var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityString}&appid=${apiKey}&units=imperial`
+        var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityString}&appid=${apiKey}&units=metric`
 
         $.get(url, function (res) {
             console.log(res)
-            var htmlString = `<h1>${cityString}</h1><p>Temperature: ${res.main.temp}&#8457;</p>`
-            htmlString+= `<p>Description: ${res.weather[0].description}</p><p>Wind Speed: ${res.wind.speed} mph</p>`
+            var htmlString = `<h1>${cityString}</h1><p>Nhiệt Độ: ${res.main.temp}&#8457;</p>`
+            htmlString+= `<p>Dự Báo: ${res.weather[0].description}</p><p>Tốc Độ Gió: ${res.wind.speed} km/giờ</p>`
             $('#forecast').html(htmlString);
 
         }, 'json');
         return false;
-    });
+    })
 })
